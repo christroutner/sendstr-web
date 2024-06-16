b433a54a38c6a52c700a9508c3eaff0331fc25cf
Chris Troutner <chris.troutner@gmail.com>
2024-06-16 10:37:21 -0700

    Using two relays

--- src/lib/localStorage.ts
+++ src/lib/localStorage.ts
@@ -8,7 +8,12 @@ export const getRelays = (): SettingsRelay[] =>
     window.localStorage.getItem("relays") ||
       JSON.stringify([
         {
+          // url: "wss://relay.sendstr.com",
           url: "wss://nostr-relay.psfoundation.info",
+          enabled: true,
+        },
+        {
+          url: "wss://relay.sendstr.com",
           enabled: true,
         },
       ]),
